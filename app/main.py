36936fd70d1511c62adee363f03698d4feb19372
Navodit Kuikel <navoditkuikel@gmail.com>
2026-01-06 22:26:56 -0600

    add health router and include routers

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
 
+from app.routers import health
+
 app = FastAPI()
 
-
-@app.get("/health")
-def health_check():
-    return {"status": "ok"}
+app.include_router(health.router)
