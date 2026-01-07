e9f88ad030673031a769470bcf648d78f03cfeda
Navodit Kuikel <navoditkuikel@gmail.com>
2026-01-06 18:31:16 -0600

    add environment-based configuration

---
+++ app/config.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    ENVIRONMENT: str = "local"  #Environment name: local / staging / production
+
+    DATABASE_URL: str | None = None #Database connection string (not used yet)
+
+    GITHUB_TOKEN: str | None = None #placeholder for future Github API Integration
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
